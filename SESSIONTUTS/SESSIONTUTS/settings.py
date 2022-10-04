56425419eed681b9452583d218f39fe10f0ddfe6
Ashok Kumar <ashokkumarbediapci@gmail.com>
2022-10-04 20:28:49 +0530

    SESSION ENGINE override

--- SESSIONTUTS/SESSIONTUTS/settings.py
+++ SESSIONTUTS/SESSIONTUTS/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+# for file based session override session engine\
+
+SESSION_ENGINE = "django.contrib.sessions.backends.file"
